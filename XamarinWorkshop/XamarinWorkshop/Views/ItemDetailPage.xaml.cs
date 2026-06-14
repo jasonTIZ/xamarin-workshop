9a8053e509567651d8e0b252ca581010e741619d
Jason Madrigal O <jasonmadrigal11@gmail.com>
2026-06-13 19:52:57 -0600

    Xamarin project

---
+++ XamarinWorkshop/XamarinWorkshop/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinWorkshop.ViewModels;
+
+namespace XamarinWorkshop.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ XamarinWorkshop/XamarinWorkshop/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinWorkshop.Models;
+using XamarinWorkshop.ViewModels;
+
+namespace XamarinWorkshop.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
